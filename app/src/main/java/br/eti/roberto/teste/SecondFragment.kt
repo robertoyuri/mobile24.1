f722d0e880d9fc52083cafbe81fc8077b9188b0c
Roberto Franco <roberto.yuri.franco@gmail.com>
2024-08-30 21:37:40 -0300

    Criação do projeto Criação de activity

---
+++ app/src/main/java/br/eti/roberto/teste/SecondFragment.kt
@@ -0,0 +1,52 @@
+package br.eti.roberto.teste
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import br.eti.roberto.teste.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener{
+            Toast.makeText(this.activity, "Olá Mundo", Toast.LENGTH_LONG).show()
+            val intent = Intent(this.activity, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
